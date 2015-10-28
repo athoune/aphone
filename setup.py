938a9efa3d1e96644b5afcfd65ff5143341567be
Mathieu Lecarme <mlecarme@bearstech.com>
2015-10-28 22:31:41 +0100

    Fix: PEP8.

--- setup.py
+++ setup.py
@@ -5,15 +5,14 @@ from setuptools import setup
 # http://pypi.python.org/pypi?%3Aaction=list_classifiers
 
 setup(name='aphone',
-    version='0.0.7',
-    package_dir={'': 'src'},
-    url='http://github.com/athoune/aphone',
-    #scripts=[],
-    description="Reading aspell phonetic grammar rules.",
-    long_description="""
-First step to build phonetic filter in any languages.
-""",
-    classifiers=[
+      version='0.0.7',
+      package_dir={'': 'src'},
+      url='http://github.com/athoune/aphone',
+      description="Reading aspell phonetic grammar rules.",
+      long_description="""
+      First step to build phonetic filter in any languages.
+      """,
+      classifiers=[
           'Development Status :: 3 - Alpha',
           'Environment :: Console',
           'Intended Audience :: Developers',
@@ -24,12 +23,12 @@ First step to build phonetic filter in any languages.
           'Topic :: Text Processing :: Linguistic',
           'Topic :: Text Processing :: Filters',
           'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
-        ],
-    license="GPLv3",
-    author="Mathieu Lecarme",
-    packages=['aphone'],
-    keywords=["phonetic", "aspell"],
-    scripts=['script/aphone'],
-    zip_safe=True,
-    install_requires=[],
-)
+      ],
+      license="GPLv3",
+      author="Mathieu Lecarme",
+      packages=['aphone'],
+      keywords=["phonetic", "aspell"],
+      scripts=['script/aphone'],
+      zip_safe=True,
+      install_requires=[],
+      )
